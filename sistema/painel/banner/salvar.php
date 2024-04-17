1c8c687633cd33b98253b28ddbf90e72beb3ab73
Micael_Henrique <133661765+Mikhailcherniev@users.noreply.github.com>
2024-04-17 09:29:14 -0300

    e

--- sistema/painel/banner/salvar.php
+++ sistema/painel/banner/salvar.php
@@ -1,30 +1,30 @@
-<?php 
+<?php
 require_once("../../conexao.php");
 $tabela = 'banner';
 $id = $_POST['id'];
-$titulo = $_POST['titulo'];
+$título = $_POST['título'];
 $subtitulo = $_POST['subtitulo'];
 
 $query = $pdo->query("SELECT * FROM $tabela where id = '$id'");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
-if(@count($res) > 0){
+if(@contagem($res) > 0){
 	$foto = $res[0]['imagem'];
-}else{
+}outro{
 	$foto = "sem-foto.jpg";
 }
 
 
 //SCRIPT PARA SUBIR FOTO NO SERVIDOR
-$nome_img = date('d-m-Y H:i:s') .'-'.@$_FILES['foto']['name'];
+$nome_img = data('dmY H:i:s') .'-'.@$_FILES['foto']['nome'];
 $nome_img = preg_replace('/[ :]+/' , '-' , $nome_img);
 
 $caminho = '../../img/banners/' .$nome_img;
 
-$imagem_temp = @$_FILES['foto']['tmp_name']; 
+$imagem_temp = @$_FILES['foto']['tmp_name'];
 
-if(@$_FILES['foto']['name'] != ""){
+if(@$_FILES['foto']['nome'] != ""){
 	$ext = pathinfo($nome_img, PATHINFO_EXTENSION);   
-	if($ext == 'png' or $ext == 'jpg' or $ext == 'jpeg' or $ext == 'gif' or $ext == 'webp'){ 
+	if($ext == 'png' ou $ext == 'jpg' ou $ext == 'jpeg' ou $ext == 'gif' ou $ext == 'webp'){
 	
 			//EXCLUO A FOTO ANTERIOR
 			if($foto != "sem-foto.jpg"){
@@ -34,15 +34,15 @@ if(@$_FILES['foto']['name'] != ""){
 			$foto = $nome_img;
 		
 		move_uploaded_file($imagem_temp, $caminho);
-	}else{
-		echo 'Extensão de Imagem não permitida!';
-		exit();
+	}outro{
+		echo 'Extensão de imagem não permitida!';
+		saída();
 	}
 }
 
 if($id == ""){
 	$pdo->query("INSERT INTO $tabela SET titulo = '$titulo', subtitulo = '$subtitulo', imagem = '$foto'");
-}else{
+}outro{
 	$pdo->query("UPDATE $tabela SET titulo = '$titulo', subtitulo = '$subtitulo', imagem = '$foto' WHERE id = '$id'");
 }
 
